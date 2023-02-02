3d877b95ac2e5f59c6a9c50c74e250c5423ce75f
Arjan Gupta <arjangupta95@gmail.com>
2023-02-01 23:19:21 -0600

    Define robot for week4

--- week4/cspace.m
+++ week4/cspace.m
@@ -1,10 +1,30 @@
 close all; clear;
 
+% ROBOT
+link_length1 = 1;
+link_length2 = 1;
+theta1 = 0;
+theta2 = 0;
+twolink_robot = rigidBodyTree;
+body1 = rigidBody('body1');
+jnt1 = rigidBodyJoint('jnt1','revolute');
+body2 = rigidBody('body2');
+jnt2 = rigidBodyJoint('jnt2','revolute');
+dhparams = [link_length1 0 0 theta1
+            link_length2 0 0 theta2]; % a alpha d theta
+setFixedTransform(jnt1,dhparams(1,:),'dh');
+setFixedTransform(jnt2,dhparams(2,:),'dh');
+body1.Joint = jnt1;
+body2.Joint = jnt2;
+addBody(twolink_robot, body1, 'base');
+addBody(twolink_robot, body2, 'body1');
+showdetails(twolink_robot)
+
 fig = uifigure('Position',[100 100 600 600]);
 
 grid1 = uigridlayout(fig,[2 1]);
 
-% PLOT
+% PLOTS
 plot_panel = uipanel(grid1,"Title", "Workspace & C-Space");
 plot_grid = uigridlayout(plot_panel, [1 2]);
 ax = uiaxes(plot_grid,"XLim",[0 360],"YLim",[0 360]);
@@ -29,10 +49,6 @@ sld2 = uislider(slider_grid,...
     'Limits',[0 360],...
     'MajorTicks',[0 45 90 135 180 225 270 315 360],...
     'ValueChangedFcn',@(sld1,event) changeYVal(sld1, plt));
-
-myrobot = importrobot("rrbot.urdf.xacro", "urdf");
-show(myrobot)
-view(2)
 
 function changeYVal(sld, plt)
     set(plt,'YData',sld.Value);
