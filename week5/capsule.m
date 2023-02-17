d450325c9197f86ff7bfea371d1a2f87e85778ca
Arjan Gupta <arjan.gupta@lindsay.com>
2023-02-17 09:27:01 -0600

    Define stateSpaceDubins() before planning path

--- week5/capsule.m
+++ week5/capsule.m
@@ -2,8 +2,8 @@ clear all; close all; clc;
 [airway,color_map, transp] = imread("939-Oblique.png");
 gray_airway = rgb2gray(airway);
 inverted_airway = gray_airway == 0;
-occ_map = binaryOccupancyMap(inverted_airway);
-show(occ_map)
+occGrid = binaryOccupancyMap(inverted_airway);
+show(occGrid)
 
 % Set start and goal poses
 start = [745,762,-pi/2];
@@ -12,5 +12,17 @@ goal = [550, 668, pi];
 % Show start and goal positions of robot
 hold on
 plot(start(1),start(2),'ro')
-plot(goal(1),goal(2),'bo')
+plot(goal(1),goal(2),'mo')
 
+% Show start and goal headings.
+r = 0.5;
+plot([start(1),start(1) + r*cos(start(3))],[start(2),start(2) + r*sin(start(3))],'r-')
+plot([goal(1),goal(1) + r*cos(goal(3))],[goal(2),goal(2) + r*sin(goal(3))],'m-')
+hold off
+
+bounds = [occGrid.XWorldLimits; occGrid.YWorldLimits; [-pi pi]];
+
+ss = stateSpaceDubins(bounds);
+ss.MinTurningRadius = 0.4;
+
+% Plan the path
