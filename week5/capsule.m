5e40c0c3ef14d754173fffa68d475a9a7f33bf01
Arjan Gupta <arjan.gupta@lindsay.com>
2023-02-17 09:04:26 -0600

    Create inverted binary occupancy grid

--- week5/capsule.m
+++ week5/capsule.m
@@ -1,4 +1,6 @@
+clear all; close all; clc;
 [airway,color_map, transp] = imread("939-Oblique.png");
 gray_airway = rgb2gray(airway);
-occ_map = binaryOccupancyMap(gray_airway);
+inverted_airway = gray_airway == 0;
+occ_map = binaryOccupancyMap(inverted_airway);
 show(occ_map)
