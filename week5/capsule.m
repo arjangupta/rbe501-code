e35e82e20e25d4b03e4b92340ed7ad444d6cde3c
Arjan Gupta <arjan.gupta@lindsay.com>
2023-02-17 09:20:03 -0600

    Show start and goal positions of robot

--- week5/capsule.m
+++ week5/capsule.m
@@ -4,3 +4,13 @@ gray_airway = rgb2gray(airway);
 inverted_airway = gray_airway == 0;
 occ_map = binaryOccupancyMap(inverted_airway);
 show(occ_map)
+
+% Set start and goal poses
+start = [745,762,-pi/2];
+goal = [550, 668, pi];
+
+% Show start and goal positions of robot
+hold on
+plot(start(1),start(2),'ro')
+plot(goal(1),goal(2),'bo')
+
