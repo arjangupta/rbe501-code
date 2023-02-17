ec6a81f24c8d591b7a0f1ee7bad6bf6ac426d926
Arjan Gupta <arjan.gupta@lindsay.com>
2023-02-17 10:15:14 -0600

    Add more code from example website
    https://www.mathworks.com/help/nav/ug/plan-mobile-robot-paths-using-rrt.html

--- week5/capsule.m
+++ week5/capsule.m
@@ -1,7 +1,11 @@
 clear all; close all; clc;
+
+% Load in the airway image, transform to grayscale and invert it
 [airway,color_map, transp] = imread("939-Oblique.png");
 gray_airway = rgb2gray(airway);
 inverted_airway = gray_airway == 0;
+
+% Create occupancy grid
 occGrid = binaryOccupancyMap(inverted_airway);
 show(occGrid)
 
@@ -26,3 +30,39 @@ ss = stateSpaceDubins(bounds);
 ss.MinTurningRadius = 0.4;
 
 % Plan the path
+stateValidator = validatorOccupancyMap(ss); 
+stateValidator.Map = occGrid;
+stateValidator.ValidationDistance = 0.05;
+
+planner = plannerRRT(ss,stateValidator);
+planner.MaxConnectionDistance = 2.0;
+planner.MaxIterations = 30000;
+
+planner.GoalReachedFcn = @exampleHelperCheckIfGoal;
+
+rng default
+
+[pthObj, solnInfo] = plan(planner,start,goal);
+
+show(occGrid)
+hold on
+
+% Plot entire search tree.
+plot(solnInfo.TreeData(:,1),solnInfo.TreeData(:,2),'.-');
+
+% Interpolate and plot path.
+interpolate(pthObj,300)
+plot(pthObj.States(:,1),pthObj.States(:,2),'r-','LineWidth',2)
+
+% Show start and goal in grid map.
+plot(start(1),start(2),'ro')
+plot(goal(1),goal(2),'mo')
+hold off
+
+function isReached = exampleHelperCheckIfGoal(planner, goalState, newState)
+    isReached = false;
+    threshold = 0.1;
+    if planner.StateSpace.distance(newState, goalState) < threshold
+        isReached = true;
+    end
+end
