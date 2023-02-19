0a417cc9340e46046344ea6dd6d7a7fe13db27fa
Arjan Gupta <arjan.gupta@lindsay.com>
2023-02-19 13:01:52 -0600

    Put functions in different files

---
+++ week5/checkIfReachedGoal.m
@@ -0,0 +1,10 @@
+function isReached = checkIfReachedGoal(planner, goalState, newState)
+    resolution = 10000;
+    isReached = false;
+    threshold = 20/resolution;
+    planner.StateSpace.distance(newState, goalState)
+%     if planner.StateSpace.
+    if planner.StateSpace.distance(newState, goalState) < threshold
+        isReached = true;
+    end
+end
