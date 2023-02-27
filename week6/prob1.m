66291bdbded3012f59c485debd306ab5e79b739f
Arjan Gupta <arjan.gupta@lindsay.com>
2023-02-27 16:11:22 -0600

    Almost compute all KEs of prob1

--- week6/prob1.m
+++ week6/prob1.m
@@ -1,7 +1,13 @@
 clear; close all; clc;
 
-syms r theta r_dot
+syms r theta r_dot theta_dot
 
 r_vec = [r*cos(theta); r*sin(theta)];
 
-r_dot_vec = [r_dot]
+r_prime_vec = ...
+[r_dot*cos(theta) - r*theta_dot*sin(theta);
+ r_dot*sin(theta) - r*theta_dot*cos(theta)];
+
+r_sq = dot(r_prime_vec, r_prime_vec);
+
+simplify(r_sq)
