1e042bf3adc7774cfeab87a881fdcf4a4670faa5
Arjan Gupta <arjangupta95@gmail.com>
2023-03-12 10:32:16 -0500

    Find a way to differentiate r and theta as fns of t in the same expression

--- week6/prob1.m
+++ week6/prob1.m
@@ -1,13 +1,8 @@
 clear; close all; clc;
 
-syms r theta r_dot theta_dot
+syms r(t) theta(t)
 
 r_vec = [r*cos(theta); r*sin(theta)];
-
-r_prime_vec = ...
-[r_dot*cos(theta) - r*theta_dot*sin(theta);
- r_dot*sin(theta) - r*theta_dot*cos(theta)];
-
-r_sq = dot(r_prime_vec, r_prime_vec);
-
-simplify(r_sq)
+r_dot = diff(r_vec,t);
+r_sq = r_dot.'*r_dot;
+latex(r_sq)
